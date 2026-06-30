84d9ae488562d22bdb68b5ae42ba210006c5c011
dzmitrybeinia <155475076+dzmitrybeinia@users.noreply.github.com>
2026-06-30 15:24:43 +0200

    Add: fruit-into-baskets - submission-0

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        l, r = 0, 0
+        res = 0
+        freq = defaultdict(int)
+        while r < len(fruits):
+            freq[fruits[r]] += 1
+            while len(freq) > 2:
+                freq[fruits[l]] -= 1
+                if(freq[fruits[l]] == 0):
+                    del freq[fruits[l]]
+                l += 1
+            res = max(res, r - l + 1)
+            r += 1
+        return res
