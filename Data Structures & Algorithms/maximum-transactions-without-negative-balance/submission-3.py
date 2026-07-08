abfa695a2faff4e64a3276cca8e75119b59b78d6
dzmitrybeinia <155475076+dzmitrybeinia@users.noreply.github.com>
2026-07-08 07:32:16 +0200

    Add: maximum-transactions-without-negative-balance - submission-3

---
+++ Data Structures & Algorithms/maximum-transactions-without-negative-balance/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxTransactions(self, transactions: List[int]) -> int:
+        balance = 0
+        res = 0
+        for tr in transactions:
+            if tr >= 0 or balance + tr >= 0:
+                balance += tr
+                res += 1
+        return res
